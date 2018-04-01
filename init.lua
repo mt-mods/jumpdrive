f58b4ef1fed1397d6c15330b045c15e9e2320fb5
Thomas Rudin <thomas@rudin.io>
2018-04-01 17:32:13 +0200

    formspec

--- init.lua
+++ init.lua
@@ -4,7 +4,13 @@ minetest.register_node("jumpdrive:engine", {
 	tiles = {"bluebeacon.png"},
 	light_source = 13,
 	groups = {cracky=3,oddly_breakable_by_hand=3},
-	drop = "jumpdrive:engine"
+	drop = "jumpdrive:engine",
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		-- meta:set_string("formspec", default.gui_bg .. default.gui_bg_img .. default.gui_slots)
+		meta:set_string("formspec", "size[8,9;]" ..
+			"list[current_player;main;0,5;8,4;]")
+	end,
 })
 
 print("[OK] Jumpdrive")
