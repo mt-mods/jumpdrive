bd3119759843b41db30ff6d31217c0f46aad4c36
Thomas Rudin <thomas@rudin.io>
2018-04-04 22:52:23 +0200

    rename texture

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ end
 
 minetest.register_node("jumpdrive:engine", {
 	description = "Jumpdrive",
-	tiles = {"bluebeacon.png"},
+	tiles = {"jumpdrive.png"},
 	light_source = 13,
 	groups = {cracky=3,oddly_breakable_by_hand=3},
 	drop = "jumpdrive:engine",
