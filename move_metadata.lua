dfcceb224516954c9c8c4f2cad9c50340cdb07a0
Thomas Rudin <thomas@rudin.io>
2018-12-05 09:09:29 +0100

    fix #28 clear metadata after jump

--- move_metadata.lua
+++ move_metadata.lua
@@ -5,8 +5,14 @@ jumpdrive.move_metadata = function(source_pos1, source_pos2, delta_vector)
 	for _,source_pos in pairs(meta_pos_list) do
 		local target_pos = vector.add(source_pos, delta_vector)
 
-		local source_meta = minetest.get_meta(source_pos):to_table()
-		minetest.get_meta(target_pos):from_table(source_meta)
+		local source_meta = minetest.get_meta(source_pos)
+		local source_table = source_meta:to_table()
+
+		-- copy metadata to target
+		minetest.get_meta(target_pos):from_table(source_table)
+
+		-- clear metadata in source
+		source_meta:from_table(nil)
 
 		local node = minetest.get_node(source_pos)
 
