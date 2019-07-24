9fa15a2b310648ee2b5b9384c6d3ad90aadf7a8b
NatureFreshMilk <naturefreshmilk@github.com>
2019-07-24 08:54:41 +0200

    allow jumping of the forcefield but not the emitter_on

--- blacklist.lua
+++ blacklist.lua
@@ -1,7 +1,6 @@
 
 if minetest.get_modpath("technic") then
 	table.insert(jumpdrive.blacklist, "technic:forcefield_emitter_on")
-	table.insert(jumpdrive.blacklist, "technic:forcefield")
 end
 
 -- TODO bedrock, advtrains tracks
