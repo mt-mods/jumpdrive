487401d884c3e6eab3af1389f54805640030c598
OgelGames <olliverdc28@gmail.com>
2019-12-18 17:57:36 +1100

    better formspec for fleet controller

--- fleet_controller.lua
+++ fleet_controller.lua
@@ -1,38 +1,36 @@
 
 local update_formspec = function(meta, pos)
 
-	local active = meta:get_int("active")
-
 	local button_line =
-		"button_exit[0,2;2,1;jump;Jump]" ..
-		"button_exit[2,2;2,1;show;Show]" ..
-		"button_exit[4,2;2,1;save;Save]" ..
-		"button[6,2;2,1;reset;Reset]"
+		"button_exit[0,1.5;2,1;jump;Jump]" ..
+		"button_exit[2,1.5;2,1;show;Show]" ..
+		"button_exit[4,1.5;2,1;save;Save]" ..
+		"button[6,1.5;2,1;reset;Reset]"
 
-	if active == 1 then
+	if meta:get_int("active") == 1 then
 		local jump_index = meta:get_int("jump_index")
 		local jump_list = minetest.deserialize( meta:get_string("jump_list") )
 
 		meta:set_string("infotext", "Controller active: " .. jump_index .. "/" .. #jump_list)
 
-		button_line = "button_exit[0,2;8,1;stop;Stop]"
+		button_line = "button_exit[0,1.5;8,1;stop;Stop]"
 	else
 		meta:set_string("infotext", "Ready")
 	end
 
-	meta:set_string("formspec", "size[8,10;]" ..
-		"field[0,1;2,1;x;X;" .. meta:get_int("x") .. "]" ..
-		"field[3,1;2,1;y;Y;" .. meta:get_int("y") .. "]" ..
-		"field[6,1;2,1;z;Z;" .. meta:get_int("z") .. "]" ..
+	meta:set_string("formspec", "size[8,9.3;]" ..
+		"field[0.3,0.5;2,1;x;X;" .. meta:get_int("x") .. "]" ..
+		"field[3.3,0.5;2,1;y;Y;" .. meta:get_int("y") .. "]" ..
+		"field[6.3,0.5;2,1;z;Z;" .. meta:get_int("z") .. "]" ..
 
 		button_line ..
 
-		"list[context;main;0,3;8,1;]" ..
+		"list[context;main;0,3.75;8,1;]" ..
 
-		"button[0,4;4,1;write_book;Write to book]" ..
-		"button[4,4;4,1;read_book;Read from book]" ..
+		"button[0,2.5;4,1;write_book;Write to book]" ..
+		"button[4,2.5;4,1;read_book;Read from book]" ..
 
-		"list[current_player;main;0,5;8,4;]" ..
+		"list[current_player;main;0,5.5;8,4;]" ..
 
 		-- listring stuff
 		"listring[]")
