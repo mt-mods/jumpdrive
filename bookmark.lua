07f5e943dfcf6a7c0fa898d027ae98f39b765eab
SwissalpS <Luke@SwissalpS.ws>
2020-10-12 14:30:55 +0200

    added compass as bookmark
    Now the bookmark handling is more intuitive.
    The first valid item from the right is used and
    put in the first free slot from the left.
    No more prioritizing of books over wands.
    
    Also will not halt when encountering a book
    not containing valid data. The next valid item
    is searched for.

--- bookmark.lua
+++ bookmark.lua
@@ -32,76 +32,111 @@ jumpdrive.write_to_book = function(pos, sender)
 
 end
 
+local function hasNil(tPos)
+	if nil == tPos
+		or nil == tPos.x
+		or nil == tPos.y
+		or nil == tPos.z
+	then
+		return true
+	end
+	return false
+end
+
+local function sanitizeAndSetCoordinates(meta, tPos)
+	meta:set_int("x", jumpdrive.sanitize_coord(tPos.x))
+	meta:set_int("y", jumpdrive.sanitize_coord(tPos.y))
+	meta:set_int("z", jumpdrive.sanitize_coord(tPos.z))
+end
+
 jumpdrive.read_from_book = function(pos)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
 	local player_name = meta:get_string("owner")
 
-	if inv:contains_item("main", {name="default:book_written", count=1}) then
-		local stack = inv:remove_item("main", {name="default:book_written", count=1})
-		local stackMeta = stack:get_meta()
+	local iSize = inv:get_size("main")
+	local stack
+	local stackName
+	local stackMeta
+	local text
+	local target_pos
+	for i = iSize, 1, -1 do
+		stack = inv:get_stack("main", i)
+		stackName = stack:get_name()
+		if "default:book_written" == stackName then
+			-- remove item from inventory
+			inv:set_stack("main", i, ItemStack())
+			stackMeta = stack:get_meta()
+			text = stackMeta:get_string("text")
+			local data = minetest.deserialize(text)
+			if hasNil(data) then
+				-- put book back where it was, it may contain other information
+				inv:set_stack("main", i, stack)
+			else
 
-		local text = stackMeta:get_string("text")
-		local data = minetest.deserialize(text)
+				target_pos = {
+					x = tonumber(data.x),
+					y = tonumber(data.y),
+					z = tonumber(data.z)
+				}
 
-		if data == nil then
-			-- put book back, it may contain other information
-			inv:add_item("main", stack)
-			-- alert player
-			if nil ~= player_name then
-				minetest.chat_send_player(player_name, "Invalid data")
+				if hasNil(target_pos) then
+					-- put book back where it was, it may contain other information
+					inv:set_stack("main", i, stack)
+					-- alert player
+					if nil ~= player_name then
+						minetest.chat_send_player(player_name, "Invalid coordinates")
+					end
+					return
+				end
+				sanitizeAndSetCoordinates(meta, target_pos)
+				-- put book back to next free slot
+				inv:add_item("main", stack)
+				return
 			end
-			return
-		end
 
-		local x = tonumber(data.x)
-		local y = tonumber(data.y)
-		local z = tonumber(data.z)
+		elseif "missions:wand_position" == stackName then
+			-- remove item from inventory
+			inv:set_stack("main", i, ItemStack())
+			stackMeta = stack:get_meta()
 
-		if x == nil or y == nil or z == nil then
-			-- put book back, it may contain other information
-			inv:add_item("main", stack)
-			-- alert player
-			if nil ~= player_name then
-				minetest.chat_send_player(player_name, "Invalid coordinates")
+			text = stackMeta:get_string("pos")
+			target_pos = minetest.string_to_pos(text)
+
+			if hasNil(target_pos) then
+				-- put wand back where it was.
+				-- In singleplayer/creative you can get an invalid position wand
+				inv:set_stack("main", i, stack)
+			else
+				-- don't know how you could get unsanitary coords in a wand,
+				-- let's just be safe
+				sanitizeAndSetCoordinates(meta, target_pos)
+				-- put wand back to next free slot
+				inv:add_item("main", stack)
+				return
 			end
-			return
-		end
 
-		meta:set_int("x", jumpdrive.sanitize_coord(x))
-		meta:set_int("y", jumpdrive.sanitize_coord(y))
-		meta:set_int("z", jumpdrive.sanitize_coord(z))
+		elseif "ccompass:" == stackName:sub(1, 9)
+				or "compass:" == stackName:sub(1, 8) then
+			-- remove item from inventory
+			inv:set_stack("main", i, ItemStack())
+			stackMeta = stack:get_meta()
 
-		-- put book back
-		inv:add_item("main", stack)
-	elseif inv:contains_item("main", {name="missions:wand_position", count=1}) then
-		local stack = inv:remove_item("main", {name="missions:wand_position", count=1})
-		local stackMeta = stack:get_meta()
+			text = stackMeta:get_string("target_pos")
+			target_pos = minetest.string_to_pos(text)
 
-		local text = stackMeta:get_string("pos")
-		local target_pos = minetest.string_to_pos(text)
-
-		if nil == target_pos then
-			-- put wand back, I don't see a way to corrupt a wand atm
-			inv:add_item("main", stack)
-			return
-		end
-
-		local x = target_pos.x
-		local y = target_pos.y
-		local z = target_pos.z
-
-		if x == nil or y == nil or z == nil then
-			-- put wand back, I don't see a way to corrupt a wand atm
-			inv:add_item("main", stack)
-			return
-		end
-
-		meta:set_int("x", jumpdrive.sanitize_coord(x))
-		meta:set_int("y", jumpdrive.sanitize_coord(y))
-		meta:set_int("z", jumpdrive.sanitize_coord(z))
-
-		-- put wand back
-		inv:add_item("main", stack)
-	end
+			if hasNil(target_pos) then
+				-- put compass back, it is probably not calibrated
+				-- we put it at same position as we did not actually use it
+				inv:set_stack("main", i, stack)
+				target_pos = {}
+			else
+				sanitizeAndSetCoordinates(meta, target_pos)
+				-- put compass back to next free slot
+				inv:add_item("main", stack)
+				return
+			end
+		end -- switch item type
+	end -- loop inventory
 end
+
