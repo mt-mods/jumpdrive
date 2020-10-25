74f51b06555f946ac7225a449ed8c48f04ddffc5
SX <sx@minetest>
2020-10-25 17:03:14 +0200

    Full edge network detection, fix typos

--- compat/technic_networks.lua
+++ compat/technic_networks.lua
@@ -1,6 +1,7 @@
 
 local vecadd = vector.add
 local poshash = minetest.hash_node_position
+local get_node = minetest.get_node
 local cables = technic.cables
 local machines = technic.machine_tiers
 
@@ -8,26 +9,35 @@ local machines = technic.machine_tiers
 local network_node_arrays = {"PR_nodes","BA_nodes","RE_nodes"}
 local function move_network(net, delta)
 	for _,tblname in ipairs(network_node_arrays) do
-		local tbl = network[tblname]
+		local tbl = net[tblname]
 		for i=#tbl,1,-1 do
 			tbl[i] = vecadd(tbl[i], delta)
 		end
 	end
-	local new_net_id = vecadd(technic.network2pos(net.id), delta)
+	local new_net_id = poshash(vecadd(technic.network2pos(net.id), delta))
+	local new_all_nodes = {}
 	local all_nodes = net.all_nodes
 	for old_node_id, old_pos in pairs(all_nodes) do
 		local new_pos = vecadd(old_pos, delta)
 		local node_id = poshash(new_pos)
 		cables[old_node_id] = nil
-		new_all_nodes[node_id] = new_pos
 		cables[node_id] = new_net_id
+		new_all_nodes[node_id] = new_pos
 	end
 	net.all_nodes = new_all_nodes
 end
 
+local function vecadd2(a, b, c)
+	return {
+		x = a.x + b.x + c.x,
+		y = a.y + b.y + c.y,
+		z = a.z + b.z + c.z,
+	}
+end
+
 local function add_edge_cable_net(pos, size, delta, t)
 	-- Find connected positions outside edge of box from given position
-	-- NOTE: At source position firt found would be enough but not at target
+	-- NOTE: At source position first found would be enough but not at target
 	local x = pos.x == 0 and -1 or (pos.x == size.x and 1)
 	local y = pos.y == 0 and -1 or (pos.y == size.y and 1)
 	local z = pos.z == 0 and -1 or (pos.z == size.z and 1)
@@ -46,6 +56,7 @@ local function add_edge_cable_net(pos, size, delta, t)
 		local net_id = cables[poshash(v)]
 		if net_id then t[net_id] = true end
 	end
+	return x, y, z
 end
 
 jumpdrive.technic_network_compat = function(source_pos1, source_pos2, target_pos1, delta_vector)
@@ -65,17 +76,28 @@ jumpdrive.technic_network_compat = function(source_pos1, source_pos2, target_pos
 				local src_pos = vecadd(source_pos1, local_pos)
 				local net_id = cables[poshash(src_pos)]
 				if net_id then
-					-- Add to (dirty) jumped networks
+					-- Add to (dirty) jumped fully contained networks
 					jump_networks[net_id] = true
-					if x==0 or x==size.x or y==0 or y==size.y or z==0 or z==size.z then
-						-- Candidate for edge network, check neighbors across border
-						add_edge_cable_net(local_pos, size, source_pos1, edge_networks)
-						add_edge_cable_net(local_pos, size, target_pos1, edge_networks)
-					end
-				elseif x==0 or x==size.x or y==0 or y==size.y or z==0 or z==size.z then
-					-- Check for unconnected nodes that might connect to network at target location
-					local name = minetest.get_node(src_pos).name
-					if machines[name] or technic.get_cable_tier(name) then
+				end
+				if x==0 or x==size.x or y==0 or y==size.y or z==0 or z==size.z then
+					-- Candidate for border crossing network, check neighbors across border
+					if net_id then
+						-- Inside network is active, check outside for active and inactive
+						local x, y, z = add_edge_cable_net(local_pos, size, source_pos1, edge_networks)
+						if x then
+							local name = get_node(vecadd2(local_pos, target_pos1, {x=x,y=0,z=0})).name
+							if machines[name] or technic.get_cable_tier(name) then edge_networks[net_id] = true end
+						end
+						if y then
+							local name = get_node(vecadd2(local_pos, target_pos1, {x=0,y=y,z=0})).name
+							if machines[name] or technic.get_cable_tier(name) then edge_networks[net_id] = true end
+						end
+						if z then
+							local name = get_node(vecadd2(local_pos, target_pos1, {x=0,y=0,z=z})).name
+							if machines[name] or technic.get_cable_tier(name) then edge_networks[net_id] = true end
+						end
+					else
+						-- Inside is inactive, check outside for active
 						add_edge_cable_net(local_pos, size, target_pos1, edge_networks)
 					end
 				end
