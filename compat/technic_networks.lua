1176ada8334bea3af61beae666e901d2ba33f55c
SX <50966843+S-S-X@users.noreply.github.com>
2020-10-31 01:53:17 +0200

    * Revert "Only remove target networks that comes into contact with source networks" (#84)
    * Basic technic network-ng compatibility
    
    * Remove extra locals
    
    * Only remove target networks that comes into contact with source networks
    
    * Revert "Only remove target networks that comes into contact with source networks"
    
    This reverts commit 935abad6441cc414b30ab184ae16dd3a3bcb3a02.

--- compat/technic_networks.lua
+++ compat/technic_networks.lua
@@ -16,18 +16,18 @@ if technic.remove_network and technic.pos2network and technic.machines then
 		local src_net_id = technic.pos2network(from_pos)
 		if src_net_id then
 			technic.remove_network(src_net_id)
+		end
 
-			-- Destroy network caches at target location, outside jump area
-			local edge = info.edge
-			for axis, value in pairs(edge) do
-				if value ~= 0 then
-					local axis_dir = {x=0,y=0,z=0}
-					axis_dir[axis] = value
-					local edge_pos = vector.add(to_pos, axis_dir)
-					local dst_net_id = technic.pos2network(edge_pos)
-					if dst_net_id then
-						technic.remove_network(dst_net_id)
-					end
+		-- Destroy network caches at target location, outside jump area
+		local edge = info.edge
+		for axis, value in pairs(edge) do
+			if value ~= 0 then
+				local axis_dir = {x=0,y=0,z=0}
+				axis_dir[axis] = value
+				local edge_pos = vector.add(to_pos, axis_dir)
+				local dst_net_id = technic.pos2network(edge_pos)
+				if dst_net_id then
+					technic.remove_network(dst_net_id)
 				end
 			end
 		end
