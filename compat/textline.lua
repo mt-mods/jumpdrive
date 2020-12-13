15075c5a60507dc0d4f139e5c81213ec85611c8e
SX <sx@minetest>
2020-12-13 12:48:05 +0200

    Better textline handling without rotating node

--- compat/textline.lua
+++ compat/textline.lua
@@ -1,13 +1,17 @@
 
 local textline_def = minetest.registered_nodes["textline:lcd"]
 assert(textline_def)
-assert(textline_def.after_place_node)
 
 -- refresh textline entities after the jump
 minetest.override_item("textline:lcd", {
 	on_movenode = function(from_pos, to_pos)
-		minetest.after(1, function()
-			textline_def.after_place_node(to_pos)
-		end)
+		local delta_vector = vector.subtract(to_pos, from_pos)
+		local objects = minetest.get_objects_inside_radius(from_pos, 0.5)
+		for _,object in ipairs(objects) do
+			local entity = object:get_luaentity()
+			if entity and entity.name == "textline:text" then
+				object:set_pos(vector.add(object:get_pos(), delta_vector))
+			end
+		end
 	end
 })
