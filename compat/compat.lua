1b5dac3a272a30211c2509009bddfc26269b02a4
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2024-12-31 20:17:30 +0100

    rewrite bed compat code (#81)
    * rewrite bed compat code
    
    * round positions
    
    * remove unused argument
    
    * add messages to asserts
    
    * cleanup whitespace, comments and MT -> core
    
    ---------
    
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
    Co-authored-by: Luke aka SwissalpS <161979+SwissalpS@users.noreply.github.com>
    Co-authored-by: Luke aka SwissalpS <Luke@SwissalpS.ws>

--- compat/compat.lua
+++ compat/compat.lua
@@ -52,8 +52,11 @@ if has_ropes_mod then
 	dofile(MP.."/compat/ropes.lua")
 end
 
+if has_beds_mod then
+	dofile(MP.."/compat/beds.lua")
+end
+
 dofile(MP.."/compat/telemosaic.lua")
-dofile(MP.."/compat/beds.lua")
 
 if has_pipeworks_mod then
 	dofile(MP.."/compat/teleporttube.lua")
@@ -74,14 +77,4 @@ end
 
 jumpdrive.commit_node_compat = function()
 	-- Nothing to do here
-end
-
-
-jumpdrive.target_region_compat = function(_, _, target_pos1, target_pos2, delta_vector)
-	-- sync compat functions
-
-	if has_beds_mod then
-		jumpdrive.beds_compat(target_pos1, target_pos2, delta_vector)
-	end
-
 end
