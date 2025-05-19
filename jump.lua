579da62f459b471d92af3dd1ada089afe19cfd34
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-05-19 05:05:31 -0500

    cache config

--- jump.lua
+++ jump.lua
@@ -80,7 +80,7 @@ jumpdrive.simulate_jump = function(pos, player, show_marker)
 	local is_empty, empty_msg = jumpdrive.is_area_empty(target_pos1, target_pos2)
 
 	if not is_empty then
-		if core.settings:get_bool("allow_jumpdrive_emerge", false) and empty_msg == "uncharted" then
+		if jumpdrive.config.emerge_uncharted and empty_msg == "uncharted" then
 			local callback = function(_, _, calls_remaining, _)
 				if calls_remaining == 0 then
 					core.chat_send_player(playername, "Charting complete!")
